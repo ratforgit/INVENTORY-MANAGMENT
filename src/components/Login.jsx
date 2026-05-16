b485087f23c073c91e2f1ad0851ff3eabb88468b
ratforgit <ratforgit@gmail.com>
2026-05-16 19:39:40 +0530

    Connected

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = ({ onLogin }) => {
   try {
     console.log('🔐 Attempting login with:', credentials.username);
     
-    const response = await fetch('http://localhost:5000/api/login', {
+    const response = await fetch('${API_BASE_URL}/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ 
